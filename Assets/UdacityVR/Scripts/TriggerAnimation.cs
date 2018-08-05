6e72c8273b30cfff0329e757814d0e150003fe32
Paul Kohler <pkohler48@gmail.com>
2018-08-05 00:03:21 -0400

    Initial Project Upload
    Entire Project Upload

---
+++ Assets/UdacityVR/Scripts/TriggerAnimation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The TriggerAnimation class activates a transition whenever the Cardboard button is pressed (or the screen touched).
+/// </summary>
+public class TriggerAnimation : MonoBehaviour
+{
+	[Tooltip ("The Animator component on this gameobject")]
+	public Animator animator;
+	[Tooltip ("The name of the Animator trigger parameter")]
+	public string triggerName;
+    [Tooltip("The AudioSource to Play when clicked")]
+    public AudioSource globeSFX;
+
+    //private int SPEED_STATE = 0;
+
+    void Start(){
+        //animator.speed = 0.0f;
+    }
+
+	void Update ()
+	{
+		// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
+		if (Input.GetMouseButtonDown (0)) {
+            //SPEED_STATE = (SPEED_STATE + 1) % 4;
+            //switch(SPEED_STATE){
+            //    case 0:
+            //        animator.speed = 0.0f;
+            //        globeSFX.pitch = 0.5f;
+            //        break;
+            //    case 1:
+            //        animator.speed = 0.5f;
+            //        globeSFX.pitch = 1.0f;
+            //        break;
+            //    case 2:
+            //        animator.speed = 1.0f;
+            //        globeSFX.pitch = 1.5f;
+            //        break;
+            //    case 3:
+            //        animator.speed = 2.0f;
+            //        globeSFX.pitch = 2.0f;
+            //        break;
+            //}
+            globeSFX.pitch = 1.0f;
+            globeSFX.Play();
+			animator.SetTrigger (triggerName);
+		}
+	}
+}
---
+++ README.txt
@@ -0,0 +1,53 @@
+Unity Editor Version: 2017.4.4f1
+Google VR SDK: GVR SDK for Unity v1.100.1
+
+Build An Apartment Base Version: v4.2.0
+
+Built for iOS
+
+Assets Used:
+
+Company: Gamemaster Audio
+Asset Name: Pro Sound Collection
+Where Used: For all SFX
+
+Company: NevLext
+Asset Name: 15 Original Wood Texture
+Where Used: Table and Bookshelf textures
+
+
+In Unity editor:
+Alt+Mouse drag will rotate the camera.  Clicking will interact with the globe.
+
+In VR:
+Look at game, click cardboard button to interact with globe. 
+
+The following things were added/edited from the default project.
+
+SFX for ambience, and globe interaction.  Globe spins at different speeds on click and has different noise.
+
+Overall Layout
+
+The apartment contains at least 15 models arranged in an interesting way
+
+All models are marked as Static (except the globe)
+
+When you run the scene, the Cardboard stereoscopic view appears and functions correctly
+
+Verify that your Player Settings are correct for your device.
+
+Quality Settings
+
+There are several spotlights and/or area lights
+
+All lights are marked as Static
+
+All lights are baked and none are realtime (except for one directional light if they choose)
+
+Lightmap Settings
+
+Globe Exists
+
+Globe Spins
+
+Globe is Textured
